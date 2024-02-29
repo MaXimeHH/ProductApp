55eb10db0252434ae381e1f4a9593319f67bf55e
MaXimeHH <maxime.debruyne1@epsi.fr>
2024-02-29 15:20:25 +0100

    suppress dependency graph git

--- src/main/java/com/back/product/repository/ProductRepository.java
+++ src/main/java/com/back/product/repository/ProductRepository.java
@@ -1,13 +1,13 @@
 package com.back.product.repository;
 
 import com.back.product.model.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-public interface ProductRepository extends JpaRepository<Product, Long> {
+public interface ProductRepository extends CrudRepository<Product, Long> {
 
     public List<Product> getAllProducts();
 
